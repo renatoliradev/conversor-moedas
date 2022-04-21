4a85b9b84f04899c67269f0f60f3e685b274132f
Renato Lira <renatoliradev@gmail.com>
2022-04-21 12:09:21 -0300

    project done

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,5 @@
+document.querySelector('#inputValueToConvert').focus()
+
 //#region VALIDATE SELECTS
 let selectConvertFrom = document.querySelector('#selectConvertFrom')
 let selectConvertTo = document.querySelector('#selectConvertTo')
@@ -11,61 +13,64 @@ let textConvertedTo = document.querySelector('#textConvertedTo')
 let valueConvertedTo = document.querySelector('#valueConvertedTo')
 
 const validateSelectionConvertFrom = () => {
-    while (selectConvertTo.length > 0) selectConvertTo.remove(0)
+    try {
+        while (selectConvertTo.length > 0) selectConvertTo.remove(0)
 
-    if (selectConvertFrom.value === "fromReal") {
-        imgConvertedFrom.src = "./assets/img/real.png"
-        textConvertedFrom.textContent = "Real Brasileiro"
-        valueConvertedFrom.textContent = ""
-        selectConvertTo.options[0] = new Option('$ Dólar Americano', 'toDolar');
-        selectConvertTo.options[1] = new Option('€ Euro', 'toEuro');
-        selectConvertTo.options[2] = new Option('BTC Bitcoin', 'toBitcoin');
+        if (selectConvertFrom.value === "fromReal") {
+            imgConvertedFrom.src = "./assets/img/real.png"
+            valueConvertedFrom.textContent = "R$ 0,00"
+            selectConvertTo.options[0] = new Option('$ Dólar Americano', 'toDolar');
+            selectConvertTo.options[1] = new Option('€ Euro', 'toEuro');
+            selectConvertTo.options[2] = new Option('BTC Bitcoin', 'toBitcoin');
+        }
+        else if (selectConvertFrom.value === "fromDolar") {
+            imgConvertedFrom.src = "./assets/img/dolar.png"
+            valueConvertedFrom.textContent = "$0,00"
+            selectConvertTo.options[0] = new Option('R$ Real Brasileiro', 'toReal');
+            selectConvertTo.options[1] = new Option('€ Euro', 'toEuro');
+            selectConvertTo.options[2] = new Option('BTC Bitcoin', 'toBitcoin');
+        }
+        else if (selectConvertFrom.value === "fromEuro") {
+            imgConvertedFrom.src = "./assets/img/euro.png"
+            valueConvertedFrom.textContent = "0,00 €"
+            selectConvertTo.options[0] = new Option('R$ Real Brasileiro', 'toReal');
+            selectConvertTo.options[1] = new Option('$ Dólar Americano', 'toDolar');
+            selectConvertTo.options[2] = new Option('BTC Bitcoin', 'toBitcoin');
+        }
+        else {
+            selectConvertTo.options[0] = new Option('R$ Real Brasileiro', 'toReal');
+            selectConvertTo.options[1] = new Option('$ Dólar Americano', 'toDolar');
+            selectConvertTo.options[2] = new Option('€ Euro', 'toEuro');
+            selectConvertTo.options[3] = new Option('BTC Bitcoin', 'toBitcoin');
+        }
     }
-    else if (selectConvertFrom.value === "fromDolar") {
-        imgConvertedFrom.src = "./assets/img/dolar.png"
-        textConvertedFrom.textContent = "Dólar Americano"
-        valueConvertedFrom.textContent = "$ 0,00"
-        selectConvertTo.options[0] = new Option('R$ Real Brasileiro', 'toReal');
-        selectConvertTo.options[1] = new Option('€ Euro', 'toEuro');
-        selectConvertTo.options[2] = new Option('BTC Bitcoin', 'toBitcoin');
+    finally {
+        validateSelectionConvertTo()
     }
-    else if (selectConvertFrom.value === "fromEuro") {
-        imgConvertedFrom.src = "./assets/img/euro.png"
-        textConvertedFrom.textContent = "Euro"
-        valueConvertedFrom.textContent = "0,00 €"
-        selectConvertTo.options[0] = new Option('R$ Real Brasileiro', 'toReal');
-        selectConvertTo.options[1] = new Option('$ Dólar Americano', 'toDolar');
-        selectConvertTo.options[2] = new Option('BTC Bitcoin', 'toBitcoin');
-    }
-    else {
-        selectConvertTo.options[0] = new Option('R$ Real Brasileiro', 'toReal');
-        selectConvertTo.options[1] = new Option('$ Dólar Americano', 'toDolar');
-        selectConvertTo.options[2] = new Option('€ Euro', 'toEuro');
-        selectConvertTo.options[3] = new Option('BTC Bitcoin', 'toBitcoin');
-    }
-    validateSelectionConvertTo()
 }
 
 const validateSelectionConvertTo = () => {
-    if (selectConvertTo.value === "toReal") {
-        imgConvertedTo.src = "./assets/img/real.png"
-        textConvertedTo.textContent = "Real Brasileiro"
-        valueConvertedTo.textContent = "R$ 0,00"
+    try {
+        if (selectConvertTo.value === "toReal") {
+            imgConvertedTo.src = "./assets/img/real.png"
+            valueConvertedTo.textContent = "R$ 0,00"
+        }
+        else if (selectConvertTo.value === "toDolar") {
+            imgConvertedTo.src = "./assets/img/dolar.png"
+            valueConvertedTo.textContent = "$0,00"
+        }
+        else if (selectConvertTo.value === "toEuro") {
+            imgConvertedTo.src = "./assets/img/euro.png"
+            valueConvertedTo.textContent = "0,00 €"
+        }
+        else {
+            imgConvertedTo.src = "./assets/img/bitcoin.png"
+            valueConvertedTo.textContent = "BTC 0"
+        }
     }
-    else if (selectConvertTo.value === "toDolar") {
-        imgConvertedTo.src = "./assets/img/dolar.png"
-        textConvertedTo.textContent = "Dólar Americano"
-        valueConvertedTo.textContent = "$ 0,00"
-    }
-    else if (selectConvertTo.value === "toEuro") {
-        imgConvertedTo.src = "./assets/img/euro.png"
-        textConvertedTo.textContent = "Euro"
-        valueConvertedTo.textContent = "0,00 €"
-    }
-    else {
-        imgConvertedTo.src = "./assets/img/bitcoin.png"
-        textConvertedTo.textContent = "Bitcoin"
-        valueConvertedTo.textContent = "BTC 0,00"
+    finally {
+
+        convertCurrency()
     }
 }
 
@@ -79,53 +84,68 @@ async function convertCurrency() {
         const response = await fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL,BRL-USD,EUR-USD,BTC-USD,BRL-EUR,USD-EUR,BTC-EUR');
         const data = await response.json();
 
-        let realToDolar = data.USDBRL.ask.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-        let realtoEuro = data.EURBRL.ask
-        let realtoBitcoin = data.BTCBRL.ask
+        let inputValueToConvert = Number(document.querySelector('#inputValueToConvert').value.replace(',', '.'))
 
-        let dolarToReal = data.BRLUSD.ask
-        let dolarToEuro = data.EURUSD.ask
-        let dolarToBitcoin = data.BTCUSD.ask
+        let realToDolar = (data.USDBRL.ask * inputValueToConvert)
+        let realtoEuro = (data.EURBRL.ask * inputValueToConvert)
+        let realtoBitcoin = (data.BTCBRL.ask * inputValueToConvert)
 
-        let euroToReal = data.BRLEUR.ask
-        let euroToDolar = data.USDEUR.ask
-        let euroToBitcoin = data.BTCEUR.ask
+        let dolarToReal = (data.BRLUSD.ask * inputValueToConvert)
+        let dolarToEuro = (data.EURUSD.ask * inputValueToConvert)
+        let dolarToBitcoin = (data.BTCUSD.ask * inputValueToConvert)
 
+        let euroToReal = (data.BRLEUR.ask * inputValueToConvert)
+        let euroToDolar = (data.USDEUR.ask * inputValueToConvert)
+        let euroToBitcoin = (data.BTCEUR.ask * inputValueToConvert)
 
         if (selectConvertFrom.value === "fromReal" && selectConvertTo.value === "toDolar") {
-            var inputValueToConvert = document.querySelector('#inputValueToConvert').value
-            
-            valueConvertedFrom.innerHTML = inputValueToConvert.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-            valueConvertedTo.innerHTML = (realToDolar * inputValueToConvert)
+            valueConvertedFrom.innerHTML = inputValueToConvert.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+            valueConvertedTo.innerHTML = realToDolar.toLocaleString('en-US', { style: 'currency', currency: 'USD' })
         }
         else if (selectConvertFrom.value === "fromReal" && selectConvertTo.value === "toEuro") {
-            valueConvertedTo.textContent = (realtoEuro * inputValueToConvert)
+            valueConvertedFrom.innerHTML = inputValueToConvert.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+            valueConvertedTo.innerHTML = realtoEuro.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' })
         }
         else if (selectConvertFrom.value === "fromReal" && selectConvertTo.value === "toBitcoin") {
-            valueConvertedTo.textContent = (realtoBitcoin * inputValueToConvert)
+            valueConvertedFrom.innerHTML = inputValueToConvert.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+            valueConvertedTo.innerHTML = ('BTC ' + realtoBitcoin)
         }
         else if (selectConvertFrom.value === "fromDolar" && selectConvertTo.value === "toReal") {
-            valueConvertedTo.textContent = (dolarToReal * inputValueToConvert)
+            valueConvertedFrom.innerHTML = inputValueToConvert.toLocaleString('en-US', { style: 'currency', currency: 'USD' })
+            valueConvertedTo.innerHTML = dolarToReal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
         }
         else if (selectConvertFrom.value === "fromDolar" && selectConvertTo.value === "toEuro") {
-            console.log('conversao de dolar para euro')
+            valueConvertedFrom.innerHTML = inputValueToConvert.toLocaleString('en-US', { style: 'currency', currency: 'USD' })
+            valueConvertedTo.innerHTML = dolarToEuro.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' })
         }
         else if (selectConvertFrom.value === "fromDolar" && selectConvertTo.value === "toBitcoin") {
-            console.log('conversao de dolar para bitcoin')
+            valueConvertedFrom.innerHTML = inputValueToConvert.toLocaleString('en-US', { style: 'currency', currency: 'USD' })
+            valueConvertedTo.innerHTML = ('BTC ' + dolarToBitcoin)
         }
         else if (selectConvertFrom.value === "fromEuro" && selectConvertTo.value === "toReal") {
-            console.log('conversao de euro para real')
+            valueConvertedFrom.innerHTML = inputValueToConvert.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' })
+            valueConvertedTo.innerHTML = euroToReal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
         }
         else if (selectConvertFrom.value === "fromEuro" && selectConvertTo.value === "toDolar") {
-            console.log('conversao de euro para dolar')
+            valueConvertedFrom.innerHTML = inputValueToConvert.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' })
+            valueConvertedTo.innerHTML = euroToDolar.toLocaleString('en-US', { style: 'currency', currency: 'USD' })
         }
         else if (selectConvertFrom.value === "fromEuro" && selectConvertTo.value === "toBitcoin") {
-            console.log('conversao de euro para bitcoin')
+            valueConvertedFrom.innerHTML = inputValueToConvert.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' })
+            valueConvertedTo.innerHTML = ('BTC ' + euroToBitcoin)
         }
+        document.querySelector('#inputValueToConvert').select()
     }
     catch (err) {
-        alert(err)
+        if (inputValueToConvert < 0)
+            alert(err)
     }
 }
 
 document.querySelector('#btnConvert').addEventListener('click', convertCurrency)
+
+document.addEventListener('keypress', function (e) {
+    if (e.which == 13) {
+        convertCurrency()
+    }
+}, false);
